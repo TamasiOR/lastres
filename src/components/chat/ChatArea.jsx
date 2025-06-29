b40a3fef2c1604ba0b61e70ddd8896b15b512508
TamasiOR <tamasioctavianradu@gmail.com>
2025-06-29 11:46:09 +0300

    Updated ChatArea.jsx

--- src/components/chat/ChatArea.jsx
+++ src/components/chat/ChatArea.jsx
@@ -361,7 +361,7 @@ export default function ChatArea() {
                           position: 'absolute',
                           top: '10%',
                           right: '0',
-                          marginBottom: '2px',
+                          marginBottom: '-122px',
                           zIndex: 9999
                         }}
                       >
